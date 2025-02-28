3fcee752b641d62876ca56a0f86c0e82765aee52
Ankit Singh <ankit5599singh@gmail.com>
2025-02-28 10:34:07 +0530

    This is the First Commit in NPM Learning

---
+++ script.js
@@ -0,0 +1,32 @@
+/*fetch('https://jsonplaceholder.typicode.com/todos/1')
+	.then((response) => response.json())
+	.then((json) => console.log(json));*/
+
+import axios from 'axios';
+
+// Make a request for a user with a given ID
+axios
+	.get('https://jsonplaceholder.typicode.com/todos/1')
+	.then(function (response) {
+		// handle success
+		console.log(response.data);
+	})
+	.catch(function (error) {
+		// handle error
+		console.log(error);
+	})
+	.finally(function () {
+		// always executed
+	});
+
+// npm init
+// initializes a new Node.js project by creating a package.json file, which stores metadata, dependencies, and scripts for the project.
+
+// npm i axios or npm install axios
+// installs the axios package and its dependencies.
+
+// npm i parcel -D
+// installs Parcel as a development dependency (-D) in the project, meaning it is only needed for development and not for production.
+
+// npx parcel index.html
+// Starts a development server using Parcel to bundle and serve index.html with automatic live reloading.
